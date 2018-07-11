9a754563307427441d7d2e10e5facc2e061992c2
Masataka <yamauchi.masataka@gmail.com>
2018-07-11 20:46:28 +0900

    Initial implementation of buffer for track progress

--- MPI/laplace_mpi.c
+++ MPI/laplace_mpi.c
@@ -44,18 +44,20 @@
 
 // largest permitted change in temp (This value takes 3264 steps)
 #define MAX_TEMP_ERROR 0.01
+#define MAX_ITERATIONS 4000
+#define N_BUF_TRACK    4*MAX_ITERATIONS/100
 
 double Temperature[ROWS+2][COLUMNS+2];
 double Temperature_last[ROWS+2][COLUMNS+2];
 
 void initialize(int npes, int my_PE_num);
-void track_progress(int iter, double dt);
+void track_progress(int const iter, double* buf_track);
+void output(int my_pe, int iteration);
 
 
 int main(int argc, char *argv[]) {
 
     int i, j;
-    int max_iterations;
     int iteration=1;
     double dt;
     double start_time, stop_time, elapsed_time;
@@ -64,6 +66,9 @@ int main(int argc, char *argv[]) {
     int        my_PE_num;           // my PE number
     double     dt_global=100;       // delta t across all PEs
     MPI_Status status;              // status returned by MPI calls
+
+    double buf_track[N_BUF_TRACK];
+    int i_buf;
 
     // the usual MPI startup routines
     MPI_Init(&argc, &argv);
@@ -90,19 +95,14 @@ int main(int argc, char *argv[]) {
       //      printf("Maximum iterations [100-4000]?\n");
       //      fflush(stdout); // Not always necessary, but can be helpful
       //      scanf("%d", &max_iterations);
-      max_iterations = 4000;
-      printf("Maximum iterations = %d\n", max_iterations);
-
+      printf("Maximum iterations = %d\n", MAX_ITERATIONS);
     }
-
-    // bcast max iterations to other PEs
-    MPI_Bcast(&max_iterations, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     if (my_PE_num==0) start_time = MPI_Wtime();
 
     initialize(npes, my_PE_num);
 
-    while ( dt_global > MAX_TEMP_ERROR && iteration <= max_iterations ) {
+    while ( dt_global > MAX_TEMP_ERROR && iteration <= MAX_ITERATIONS ) {
 
         // main calculation: average my four neighbors
         for(i = 1; i <= ROWS; i++) {
@@ -149,13 +149,20 @@ int main(int argc, char *argv[]) {
 
         // periodically print test values - only for PE in lower corner
         if((iteration % 100) == 0) {
-            if (my_PE_num == npes-1){
-	      track_progress(iteration, dt_global);
-	    }
+	  if (my_PE_num == npes-1) {
+	    i_buf = 0.04 * (iteration-1);   // i_buf = (iteration/100) * 4
+	    buf_track[i_buf]   = dt_global;
+	    buf_track[i_buf+1] = Temperature[ROWS-5][COLUMNS-5];
+	    buf_track[i_buf+2] = Temperature[ROWS-4][COLUMNS-4];
+	    buf_track[i_buf+3] = Temperature[ROWS-3][COLUMNS-3];
+	    // track_progress_org(iteration, dt_global);
+	  }
         }
 
 	iteration++;
     }
+
+    track_progress(i_buf+3, buf_track);
 
     // Slightly more accurate timing and cleaner output 
     MPI_Barrier(MPI_COMM_WORLD);
@@ -205,19 +212,45 @@ void initialize(int npes, int my_PE_num){
       for (j=0; j<=COLUMNS+1; j++)
 	Temperature_last[ROWS+1][j] = (100.0/COLUMNS) * j;
 
+    // Allocate buffer for track_progress
+    
 }
 
 
 // only called by last PE
 // print diagonal in bottom right corner where most action is
-void track_progress(int iteration, double dt) {
+void track_progress(int const iter, double* buf_track) {
 
     int i;
-
-    printf("---- Iteration %d, dt = %f ----\n", iteration, dt);
-    // output global coordinates so user doesn't have to understand decompositi
-    for(i = 5; i >= 3; i--) {
-      printf("[%d,%d]: %5.2f  ", ROWS_GLOBAL-i, COLUMNS-i, Temperature[ROWS-i][COLUMNS-i]);
+    for (i = 0; i < iter; i+=4) {
+      printf("---- Iteration %d, dt = %f ----\n", (i+1)*100, buf_track[i]);
+      printf("[%d,%d]: %5.2f  [%d,%d]: %5.2f  [%d,%d]: %5.2f  ",
+	     ROWS_GLOBAL-i, COLUMNS-i, buf_track[i+1],
+	     ROWS_GLOBAL-i, COLUMNS-i, buf_track[i+2],
+	     ROWS_GLOBAL-i, COLUMNS-i, buf_track[i+3]);
     }
     printf("\n");
+}
+
+
+void output(int my_pe, int iteration) {
+  FILE* fp;
+  char filename[50];
+  sprintf(filename, "output%d.txt", iteration);
+
+  for (int pe = 0; pe<4; pe++) {
+    if (my_pe == pe) {
+      fp = fopen(filename, "a");
+
+      for(int y = 1; y <= ROWS; y++) {
+	for (int x = 1; x < COLUMNS; x++) {
+	  fprintf(fp, "%5.3f ", Temperature[y][x]);
+	}
+	fprintf(fp, "\n");
+      }
+      fflush(fp);
+      fclose(fp);
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+  }
 }
