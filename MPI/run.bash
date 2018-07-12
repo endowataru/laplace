f051d277b45c49a54f089b07d239348a970c5315
Takuma Nozawa <tnozawa@br018.pvt.bridges.psc.edu>
2018-07-12 18:09:34 -0400

    script was added

--- MPI/run.bash
+++ MPI/run.bash
@@ -6,9 +6,8 @@ module load pgi mpi/pgi_openmpi
 mpicc -O3 -mp -acc -Minfo=accel -Mipa=fast,inline laplace_mpi.c
 
 sbatch \
--N $2 -p GPU --ntasks-per-node 2 \
+-N 2 -p GPU --ntasks-per-node 2 \
 --gres=gpu:p100:2 -t 5 -A ac560tp \
---reservation=$1 \
 -J challenge \
 job.bash
 
