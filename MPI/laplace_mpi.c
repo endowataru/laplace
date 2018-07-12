a67512e1925d761e5cd36ba4828b68c8798415e7
Wataru ENDO <wendo@eidos.ic.i.u-tokyo.ac.jp>
2018-07-12 23:43:09 +0900

    Fix indentation of onaka

--- MPI/laplace_mpi.c
+++ MPI/laplace_mpi.c
@@ -68,175 +68,175 @@ int        my_PE_num;           // my PE number
 
 int main(int argc, char *argv[]) {
 
-	int i, j;
-	int max_iterations;
-	int iteration=1;
-	double dt;
-	double start_time, stop_time, elapsed_time;
+    int i, j;
+    int max_iterations;
+    int iteration=1;
+    double dt;
+    double start_time, stop_time, elapsed_time;
 
-	double     dt_global=100;       // delta t across all PEs
-	MPI_Status status;              // status returned by MPI calls
+    double     dt_global=100;       // delta t across all PEs
+    MPI_Status status;              // status returned by MPI calls
 
-	// the usual MPI startup routines
-	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &my_PE_num);
-	MPI_Comm_size(MPI_COMM_WORLD, &npes);
+    // the usual MPI startup routines
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_PE_num);
+    MPI_Comm_size(MPI_COMM_WORLD, &npes);
 
 
-	if (my_PE_num == 0)
-	{
-		printf("Running on %d MPI processes\n\n", npes);
-	}
+    if (my_PE_num == 0)
+    {
+        printf("Running on %d MPI processes\n\n", npes);
+    }
 
-	// verify only NPES PEs are being used
-	if(npes != NPES) {
-		if(my_PE_num==0) {
-			printf("This code must be run with %d PEs\n", NPES);
-		}
-		MPI_Finalize();
-		exit(1);
-	}
+    // verify only NPES PEs are being used
+    if(npes != NPES) {
+        if(my_PE_num==0) {
+            printf("This code must be run with %d PEs\n", NPES);
+        }
+        MPI_Finalize();
+        exit(1);
+    }
 
-	// PE 0 asks for input
-	if(my_PE_num==0) {
-		//      printf("Maximum iterations [100-4000]?\n");
-		//      fflush(stdout); // Not always necessary, but can be helpful
-		//      scanf("%d", &max_iterations);
-		max_iterations = 4000;
-		printf("Maximum iterations = %d\n", max_iterations);
+    // PE 0 asks for input
+    if(my_PE_num==0) {
+        //      printf("Maximum iterations [100-4000]?\n");
+        //      fflush(stdout); // Not always necessary, but can be helpful
+        //      scanf("%d", &max_iterations);
+        max_iterations = 4000;
+        printf("Maximum iterations = %d\n", max_iterations);
 
-	}
+    }
 
-	// bcast max iterations to other PEs
-	MPI_Bcast(&max_iterations, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    // bcast max iterations to other PEs
+    MPI_Bcast(&max_iterations, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-	if (my_PE_num==0) start_time = MPI_Wtime();
+    if (my_PE_num==0) start_time = MPI_Wtime();
 
-	initialize(npes, my_PE_num);
+    initialize(npes, my_PE_num);
 
-	while ( dt_global > MAX_TEMP_ERROR && iteration <= max_iterations ) {
+    while ( dt_global > MAX_TEMP_ERROR && iteration <= max_iterations ) {
 
-		for(int d=0;d<DEPTH;d++){
-			// main calculation: average my four neighborsa
+        for(int d=0;d<DEPTH;d++){
+            // main calculation: average my four neighborsa
 
-			//This code block is to surpress updates of bounderies.
-			int start_i=1+d;
-			int end_i=ROWS+DEPTH*2-2-d;
-			if(my_PE_num == 0) start_i = fmax(start_i, DEPTH);
-			if(my_PE_num == npes-1) end_i = fmin(end_i, ROWS+DEPTH-1);
-			// if(my_PE_num==2){ debug_dumpallarry(); printf("%d-%d\n",start_i,end_i);}
+            //This code block is to surpress updates of bounderies.
+            int start_i=1+d;
+            int end_i=ROWS+DEPTH*2-2-d;
+            if(my_PE_num == 0) start_i = fmax(start_i, DEPTH);
+            if(my_PE_num == npes-1) end_i = fmin(end_i, ROWS+DEPTH-1);
+            // if(my_PE_num==2){ debug_dumpallarry(); printf("%d-%d\n",start_i,end_i);}
 
-			for(i = start_i; i <= end_i; i++) {
-				for(j = 1; j <= COLUMNS; j++) {
-					Temperature[i][j] = 0.25 * (Temperature_last[i+1][j] + Temperature_last[i-1][j] + Temperature_last[i][j+1] + Temperature_last[i][j-1]);
-				}
-			}
-			if(d!=DEPTH-1){
-			for(i = start_i; i <= end_i; i++){
-				for(j = 1; j <= COLUMNS; j++){
-					Temperature_last[i][j]=Temperature[i][j];
-				}
-			}
-			}
+            for(i = start_i; i <= end_i; i++) {
+                for(j = 1; j <= COLUMNS; j++) {
+                    Temperature[i][j] = 0.25 * (Temperature_last[i+1][j] + Temperature_last[i-1][j] + Temperature_last[i][j+1] + Temperature_last[i][j-1]);
+                }
+            }
+            if(d!=DEPTH-1){
+                for(i = start_i; i <= end_i; i++){
+                    for(j = 1; j <= COLUMNS; j++){
+                        Temperature_last[i][j]=Temperature[i][j];
+                    }
+                }
+            }
 
-		}
+        }
 
-		dt = 0;
+        dt = 0;
 
-		for(i = DEPTH; i <= ROWS+DEPTH-1; i++){
-			for(j = 1; j <= COLUMNS; j++){
-				dt = fmax( fabs(Temperature[i][j]-Temperature_last[i][j]), dt);
-				Temperature_last[i][j] = Temperature[i][j];
-			}
-		}
+        for(i = DEPTH; i <= ROWS+DEPTH-1; i++){
+            for(j = 1; j <= COLUMNS; j++){
+                dt = fmax( fabs(Temperature[i][j]-Temperature_last[i][j]), dt);
+                Temperature_last[i][j] = Temperature[i][j];
+            }
+        }
 
-		// COMMUNICATION PHASE: send ghost rows for next iteration
+        // COMMUNICATION PHASE: send ghost rows for next iteration
 
-		// send bottom real row down
-		if(my_PE_num != npes-1){             //unless we are bottom PE
-			MPI_Send(&Temperature[ROWS][1], (COLUMNS+2)*DEPTH-2, MPI_DOUBLE, my_PE_num+1, DOWN, MPI_COMM_WORLD);
-		}
+        // send bottom real row down
+        if(my_PE_num != npes-1){             //unless we are bottom PE
+            MPI_Send(&Temperature[ROWS][1], (COLUMNS+2)*DEPTH-2, MPI_DOUBLE, my_PE_num+1, DOWN, MPI_COMM_WORLD);
+        }
 
-		// receive the bottom row from above into our top ghost row
-		if(my_PE_num != 0){                  //unless we are top PE
-			MPI_Recv(&Temperature_last[0][1], (COLUMNS+2)*DEPTH-2, MPI_DOUBLE, my_PE_num-1, DOWN, MPI_COMM_WORLD, &status);
-		}
+        // receive the bottom row from above into our top ghost row
+        if(my_PE_num != 0){                  //unless we are top PE
+            MPI_Recv(&Temperature_last[0][1], (COLUMNS+2)*DEPTH-2, MPI_DOUBLE, my_PE_num-1, DOWN, MPI_COMM_WORLD, &status);
+        }
 
-		// send top real row up
-		if(my_PE_num != 0){                    //unless we are top PE
-			MPI_Send(&Temperature[DEPTH][1], (COLUMNS+2)*DEPTH-2, MPI_DOUBLE, my_PE_num-1, UP, MPI_COMM_WORLD);
-		}
+        // send top real row up
+        if(my_PE_num != 0){                    //unless we are top PE
+            MPI_Send(&Temperature[DEPTH][1], (COLUMNS+2)*DEPTH-2, MPI_DOUBLE, my_PE_num-1, UP, MPI_COMM_WORLD);
+        }
 
-		// receive the top row from below into our bottom ghost row
-		if(my_PE_num != npes-1){             //unless we are bottom PE
-			MPI_Recv(&Temperature_last[ROWS+DEPTH][1], (COLUMNS+2)*DEPTH-2, MPI_DOUBLE, my_PE_num+1, UP, MPI_COMM_WORLD, &status);
-		}
+        // receive the top row from below into our bottom ghost row
+        if(my_PE_num != npes-1){             //unless we are bottom PE
+            MPI_Recv(&Temperature_last[ROWS+DEPTH][1], (COLUMNS+2)*DEPTH-2, MPI_DOUBLE, my_PE_num+1, UP, MPI_COMM_WORLD, &status);
+        }
 
-		// find global dt
-		MPI_Reduce(&dt, &dt_global, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-		MPI_Bcast(&dt_global, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        // find global dt
+        MPI_Reduce(&dt, &dt_global, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&dt_global, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-		// periodically print test values - only for PE in lower corner
-		if((iteration % 100) == 0) {
-			if (my_PE_num == npes-1 ){
-				track_progress(iteration, dt_global);
-			}
-		}
+        // periodically print test values - only for PE in lower corner
+        if((iteration % 100) == 0) {
+            if (my_PE_num == npes-1 ){
+                track_progress(iteration, dt_global);
+            }
+        }
 
-		iteration+=DEPTH;
-	}
+        iteration+=DEPTH;
+    }
 
-	// Slightly more accurate timing and cleaner output
-	MPI_Barrier(MPI_COMM_WORLD);
+    // Slightly more accurate timing and cleaner output
+    MPI_Barrier(MPI_COMM_WORLD);
 
-	// PE 0 finish timing and output values
-	if (my_PE_num==0){
-		stop_time = MPI_Wtime();
-		elapsed_time = stop_time - start_time;
+    // PE 0 finish timing and output values
+    if (my_PE_num==0){
+        stop_time = MPI_Wtime();
+        elapsed_time = stop_time - start_time;
 
-		printf("\nMax error at iteration %d was %20.15g\n", iteration-1, dt_global);
-		printf("Total time was %f seconds.\n", elapsed_time);
-	}
+        printf("\nMax error at iteration %d was %20.15g\n", iteration-1, dt_global);
+        printf("Total time was %f seconds.\n", elapsed_time);
+    }
 
-	if (CHECK_ROW/ROWS == my_PE_num) {
-		printf("PE %d: T(%d,%d) = %lf (%lf)\n",
-				my_PE_num, CHECK_ROW, CHECK_COL,
-				Temperature_last[CHECK_ROW % ROWS + DEPTH][CHECK_COL],
-				Temperature[CHECK_ROW % ROWS + DEPTH][CHECK_COL]
-				// TODO: Temperature becomes 0.0
-		      );
-	}
-	MPI_Finalize();
+    if (CHECK_ROW/ROWS == my_PE_num) {
+        printf("PE %d: T(%d,%d) = %lf (%lf)\n",
+                my_PE_num, CHECK_ROW, CHECK_COL,
+                Temperature_last[CHECK_ROW % ROWS + DEPTH][CHECK_COL],
+                Temperature[CHECK_ROW % ROWS + DEPTH][CHECK_COL]
+                // TODO: Temperature becomes 0.0
+              );
+    }
+    MPI_Finalize();
 }
 
 
 
 void initialize(){
 
-	double tMin, tMax;  //Local boundary limits
-	int i,j;
+    double tMin, tMax;  //Local boundary limits
+    int i,j;
 
-	memset(Temperature_last, 0, sizeof(Temperature_last));
+    memset(Temperature_last, 0, sizeof(Temperature_last));
 
-	// Local boundry condition endpoints
-	tMin = (my_PE_num)*100.0/npes;
-	tMax = (my_PE_num+1)*100.0/npes;
+    // Local boundry condition endpoints
+    tMin = (my_PE_num)*100.0/npes;
+    tMax = (my_PE_num+1)*100.0/npes;
 
-	// Left and right boundaries
-	for (i = 0; i < ROWS+DEPTH*2; i++) {
-		Temperature[i][0] = Temperature_last[i][0] = 0.0;
-		Temperature[i][COLUMNS+1] = Temperature_last[i][COLUMNS+1] = tMin + ((tMax-tMin)/ROWS)*(i-DEPTH+1);
-	}
+    // Left and right boundaries
+    for (i = 0; i < ROWS+DEPTH*2; i++) {
+        Temperature[i][0] = Temperature_last[i][0] = 0.0;
+        Temperature[i][COLUMNS+1] = Temperature_last[i][COLUMNS+1] = tMin + ((tMax-tMin)/ROWS)*(i-DEPTH+1);
+    }
 
-	// Top boundary (PE 0 only)
-	if (my_PE_num == 0)
-		for (j = 0; j <= COLUMNS+1; j++)
-			Temperature[DEPTH-1][j] = Temperature_last[DEPTH-1][j] = 0.0;
+    // Top boundary (PE 0 only)
+    if (my_PE_num == 0)
+        for (j = 0; j <= COLUMNS+1; j++)
+            Temperature[DEPTH-1][j] = Temperature_last[DEPTH-1][j] = 0.0;
 
-	// Bottom boundary (Last PE only)
-	if (my_PE_num == npes-1)
-		for (j=0; j<=COLUMNS+1; j++)
-			Temperature[DEPTH+ROWS][j] = Temperature_last[DEPTH+ROWS][j] = (100.0/COLUMNS) * j;
+    // Bottom boundary (Last PE only)
+    if (my_PE_num == npes-1)
+        for (j=0; j<=COLUMNS+1; j++)
+            Temperature[DEPTH+ROWS][j] = Temperature_last[DEPTH+ROWS][j] = (100.0/COLUMNS) * j;
 
 }
 
@@ -245,24 +245,24 @@ void initialize(){
 // print diagonal in bottom right corner where most action is
 void track_progress(int iteration, double dt) {
 
-	int i;
+    int i;
 
-	printf("---- Iteration %d, dt = %f ----\n", iteration, dt);
-	// output global coordinates so user doesn't have to understand decompositi
-	for(i = 5; i >= 3; i--) {
-		printf("[%d,%d]: %5.2f  ", ROWS_GLOBAL-i, COLUMNS-i, Temperature[ROWS+DEPTH-i][COLUMNS-i]);
-	}
-	printf("\n");
+    printf("---- Iteration %d, dt = %f ----\n", iteration, dt);
+    // output global coordinates so user doesn't have to understand decompositi
+    for(i = 5; i >= 3; i--) {
+        printf("[%d,%d]: %5.2f  ", ROWS_GLOBAL-i, COLUMNS-i, Temperature[ROWS+DEPTH-i][COLUMNS-i]);
+    }
+    printf("\n");
 }
 
 
 void debug_dumpallarry(){
-	for(int i =0;i<ROWS+2*DEPTH;i++){
-		for(int j=0;j<COLUMNS+2;j++){
-			printf("%5.2f,",Temperature_last[i][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
+    for(int i =0;i<ROWS+2*DEPTH;i++){
+        for(int j=0;j<COLUMNS+2;j++){
+            printf("%5.2f,",Temperature_last[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
