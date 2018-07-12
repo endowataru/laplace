88ae99d5a5a3527a567ba87e82a7f79558a9b9f9
Takuma Nozawa <tnozawa@br018.pvt.bridges.psc.edu>
2018-07-12 17:57:20 -0400

    1

--- MPI/job.bash
+++ MPI/job.bash
@@ -3,5 +3,5 @@
 module unload pgi mpi/pgi_openmpi intel
 module load pgi mpi/pgi_openmpi
 
-mpirun -n 4 ./a.out
-
+#mpirun -n 4 ./a.out
+mpirun -n 4 -genv MV2_ENABLE_AFFINITY=0 ./a.out 2>&1 | tee $PBS_JOBNAME.$PBS_JOBID.log
--- MPI/run.bash
+++ MPI/run.bash
@@ -3,11 +3,11 @@
 module unload pgi mpi/pgi_openmpi intel
 module load pgi mpi/pgi_openmpi
 
-mpicc -O3 -mp -acc -Minfo=accel laplace_mpi.c
+mpicc -O3 -mp -acc -Minfo=accel -Mipa=fast,inline laplace_mpi.c
 
 sbatch \
--N $2 -p GPU-shared --ntasks-per-node 1 \
---gres=gpu:p100:1 -t 5 -A ac560tp \
+-N $2 -p GPU --ntasks-per-node 2 \
+--gres=gpu:p100:2 -t 5 -A ac560tp \
 --reservation=$1 \
 -J challenge \
 job.bash
